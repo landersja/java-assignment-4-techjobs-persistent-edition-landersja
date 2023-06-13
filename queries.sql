5b1661d0f318a7f911783fb8d55e27666de10a8f
Jamyria <landersja@live.com>
2023-06-12 23:06:08 -0500

    final update

--- queries.sql
+++ queries.sql
@@ -1,7 +1,11 @@
 -- Part 1: Test it with SQL
-
+--ID(Integer)
+--Employer(VARCHAR)
+--Name(VARCHAR)
+--Skill(VARCHAR)
 -- Part 2: Test it with SQL
-
+--"SELECT\\s+name\\s+FROM\\s+employer\\s+WHERE\\s+location\\s+=\\s+\"St.\\s+Louis\\s+City\";"
+SELECT name FROM employer WHERE location = "St. Louis City";
 -- Part 3: Test it with SQL
 DROP TABLE job;
 -- "SELECT\\s+\\*\\s+FROM\\s+skill" +
